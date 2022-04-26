ac884b34e7ba857110ebaa7cde6b7aba22ed76d4
95chg <rellic95@gmail.com>
2022-04-26 20:59:17 +0900

    Baekjoon_step_9 : clear

---
+++ Baekjoon/Solution/Step_9/11729.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const file = process.platform === 'linux' ? '/dev/stdin' : './example.txt';
+const input = Number(fs.readFileSync(file).toString().trim());
+
+const towerOfHanoi = (num) => {
+    let count = 0;
+    let answer = '';
+
+    const movePlate = (num, start, waypoint, end) => {
+        if (!num) return;
+        else {
+            movePlate(num - 1, start, end, waypoint);
+            answer += `${start} ${end}\n`
+            count += 1;
+            movePlate(num - 1, waypoint, start, end);
+        }
+    }
+
+    movePlate(input, 1, 2, 3);
+    
+    console.log(count);
+    return answer.trim();
+};
+
+console.log(towerOfHanoi(input));
