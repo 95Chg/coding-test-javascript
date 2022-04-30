3efb24be57f16e8b26ccda767fbaf88d17d7fbde
95chg <rellic95@gmail.com>
2022-04-30 22:38:07 +0900

    Baekjoon_step_5 : clear

--- Baekjoon/Solution/Step_5/1065.js
+++ Baekjoon/Solution/Step_5/1065.js
@@ -1,3 +1,25 @@
 const fs = require('fs');
 const file = process.platform === 'linux' ? '/dev/stdin' : './example.txt';
 const input = fs.readFileSync(file).toString().trim();
+
+const n = +input
+
+console.log(solution(n));
+
+function solution(n) {
+    let answer = 0;
+
+    for (let i = 1; i <= n; i++) {
+        if (i < 100) {
+            answer++
+            continue
+        } else {
+            const splitedNumber = i.toString().split('').map(Number);
+            if (splitedNumber[0] - splitedNumber[1] === splitedNumber[1] - splitedNumber[2]) {
+                answer++
+            }
+        }
+    }
+
+    return answer;
+}
