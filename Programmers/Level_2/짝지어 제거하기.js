fd853984beb448bef765136744e36aa0222dbcf5
95chg <rellic95@gmail.com>
2022-05-26 16:27:13 +0900

    Programmers_level_2 : is proceeding

---
+++ Programmers/Level_2/짝지어 제거하기.js
@@ -0,0 +1,13 @@
+function solution(s) {
+    const stack = [];
+
+    if (s.length % 2 !== 0) return 0;
+
+    for (let i = 0; i < s.length; i += 1) {
+        stack[stack.length - 1] === s[i]
+            ? stack.pop()
+            : stack.push(s[i]);
+    }
+
+    return stack.length === 0 ? 1 : 0;
+}
